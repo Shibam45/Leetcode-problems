5f35563811618f7fe495a120348cae5551c0ba3c
Shibam Ghosh <gshibam632@gmail.com>
2025-11-26 17:35:53 +0530

    Time: 7 ms (28.20%) | Memory: 17 MB (29.89%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    set<vector<int>> s;
+    void getallcombin(vector<int>&arr, int idx, int tar, vector<vector<int>>&ans,vector<int>&combin){
+
+        if(idx == arr.size() || tar < 0){
+            return;
+        }
+
+        if(tar == 0){
+            if(s.find(combin) == s.end()){
+                ans.push_back(combin);
+                s.insert(combin);
+            }
+            return;
+        }
+
+        combin.push_back(arr[idx]);
+        // Single
+        getallcombin(arr,idx+1,tar-arr[idx],ans,combin);
+        //Multiple
+        getallcombin(arr,idx,tar-arr[idx],ans,combin);
+        combin.pop_back();
+        //Exclude
+        getallcombin(arr,idx+1,tar,ans,combin);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        vector<vector<int>>ans;
+        vector<int>combin;
+        getallcombin(arr,0,target,ans,combin);
+        return ans;
+    }
+};
